583245f022e3fe0e5eb3eb3cafe72e2e0218953d
Tach <tacdurdyhaj@gmail.com>
2025-09-05 13:17:39 +0500

    Для Githuub Secrets

--- data.py
+++ data.py
@@ -4,12 +4,16 @@ import json, os
 
 # load_dotenv()
 
-# COOKIES_JSON = os.getenv('COOKIES_JSON')
-# GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+# --- Получить из Github Secrets ---
+COOKIES_JSON = os.getenv('COOKIES_JSON')
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 
-HG_API_KEY = os.environ.get('HG_API_KEY', '')
 
-COOKIES_JSON = os.environ.get('COOKIES_JSON', '{}')
+# --- Для локальных тестов
+# HG_API_KEY = os.environ.get('HG_API_KEY', '')
+# COOKIES_JSON = os.environ.get('COOKIES_JSON', '{}')
+
+
 cookies = json.loads(COOKIES_JSON)
 
 GROUP_ID = 352
