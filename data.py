950dd0be685d584f04377738b4f35fe828cd7c76
Tach <tacdurdyhaj@gmail.com>
2025-09-12 16:22:03 +0500

    Changed User-Agent to Mozilla/5.0

--- data.py
+++ data.py
@@ -7,14 +7,11 @@ import json, os
 # --- Получить из Github Secrets ---
 COOKIES_JSON = os.getenv('COOKIES_JSON')
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
-
-
-# --- Для локальных тестов
-# HG_API_KEY = os.environ.get('HG_API_KEY', '')
-# COOKIES_JSON = os.environ.get('COOKIES_JSON', '{}')
-
-
 cookies = json.loads(COOKIES_JSON)
+
+# --- Для локальных тестов ---
+# GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY', '')
+# COOKIES_JSON = os.environ.get('COOKIES_JSON', '{}')
 
 GROUP_ID = 352
 MY_USER_ID = 8724
